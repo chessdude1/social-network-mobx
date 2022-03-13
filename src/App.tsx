ccc654657520e505abfb03ee1c8456a2ff06338c
chessdude1 <mitia.platonow2011@yandex.ru>
2022-03-13 15:40:09 +0300

    feat: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  ThemeProvider,
+  createTheme,
+  responsiveFontSizes,
+} from '@mui/material/styles';
 import './App.css';
+
+const theme = responsiveFontSizes(
+  createTheme({
+    typography: {
+      allVariants: {
+        fontFamily: 'Inter',
+        textTransform: 'none',
+      },
+    },
+  })
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <h1>Test</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
